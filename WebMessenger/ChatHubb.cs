cedc5dfa15a27a8dea74bc50116430a571ac5966
Dmitry Gaimaldinov <dmitrygaimaldinov@gmail.com>
2023-11-23 19:55:07 +0500

    Add project files.

---
+++ WebMessenger/ChatHubb.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebMessenger
+{
+    public class ChatHubb : Hub
+    {
+        private readonly ILogger<ChatHubb> _logger;
+
+        public ChatHubb(ILogger<ChatHubb> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task SendMessage(string text)
+        {
+            _logger.LogInformation(text);
+            await this.Clients.All.SendAsync("NewMessage", text);
+        }
+    }
+}
